79552da5bfff648fc3ab406247bb74062f40b4a6
Tarikvillalobos <tarik.villalobos@gmail.com>
2023-02-10 20:19:37 -0300

    Initial Commit

---
+++ Novo/NovoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovoApp.swift
+//  Novo
+//
+//  Created by Tarik Villalobos on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NovoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
